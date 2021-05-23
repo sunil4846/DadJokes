d14a9d83381a1dfc6023b848e50d2d7ce64d9720
sunil4846 <sunilrajput4846.com>
2021-05-23 20:16:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+// Promises
+// const generateJokes = () => {
+//     const setHeader = {
+//         headers: {
+//             Accept : "application/json"
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', setHeader)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         jokes.innerHTML = data.joke;
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+// }
+
+// async-await method
+
+const generateJokes = async () => {
+    try{
+        const setHeader = {
+            headers: {
+                Accept : "application/json"
+            }
+        }
+    
+        const res = await fetch('https://icanhazdadjoke.com', setHeader)    
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }
+    catch(err){
+        console.log(`the error is ${err}`);
+    }
+   
+    
+    
+
+}
+
+
+jokeBtn.addEventListener('click',generateJokes);
+generateJokes();
